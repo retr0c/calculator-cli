7021fd3807b277c51d833b00142e132f1b138cb2
John Doe <johndoe@example.com>
2024-07-30 13:33:06 -0500

    Add subtract multiply and divide method

--- calculatorf/model.py
+++ calculatorf/model.py
@@ -2,3 +2,17 @@ class Calculator:
     
     def add(self, numero1: float, numero2: float) -> float:
         return numero1 + numero2
+    
+    def subtract(self, numero1: float, numero2: float) -> float:
+        return numero1 - numero2
+    
+    def multiply(self, numero1: float, numero2: float) -> float:
+        return numero1 * numero2
+    
+    def divide(self, numero1: float, numero2: float) -> float :
+        if numero2 == 0:
+            return "Cannot divide by 0!"
+        
+        return numero1 / numero2
+    
+    
